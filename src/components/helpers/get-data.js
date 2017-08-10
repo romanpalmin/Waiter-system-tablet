073d1945137e69d26ab925245fba7a97bede757d
r.palmin <roman.palmin@gmail.com>
2017-08-10 15:53:21 +0300

    * Добавлен универсальный модуль работы с вебсервисом

---
+++ src/components/helpers/get-data.js
@@ -0,0 +1,40 @@
+import store from './../../store/index';
+import axios from 'axios';
+
+export default {
+    getData: async (options, url = store.getters.apiUrl) => {
+        const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+        async function getDataStr(uuid) {
+            const optionsUuid = {'ReqUuid': uuid};
+            try {
+                const preloadAns = await axios.get(store.getters.apiUrl, {params: optionsUuid});
+                const answer = preloadAns.data[0];
+                if (!answer || answer.status !== 1) {
+                    console.log('Следующая попытка через 2 секунды');
+                    await delay(2000);
+                    return getDataStr(optionsUuid.ReqUuid);
+                } else {
+                    return new Promise((resolve) => {
+                        const res = JSON.parse(answer.answerADM)[0];
+                        resolve(res);
+                    });
+                }
+            }
+            catch (err){
+                return new Promise((resolve, reject) => {
+                    reject(err);
+                });
+            }
+        }
+        try {
+            const preloadUuid = await axios.get(url, {params: options});
+            const recUuid = preloadUuid.data;
+            return getDataStr(recUuid);
+        }
+        catch (error) {
+            return new Promise((resolve, reject) => {
+                reject(error);
+            });
+        }
+    }
+}
